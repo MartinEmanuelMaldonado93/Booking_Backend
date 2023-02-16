70f0d09e13887e813c6da6caf008740f27af3cb0
Martin Emanuel <martinemanuel93@gmail.com>
2023-02-16 14:51:46 -0300

    delete verifytoken functions

--- src/services/verifyToken.ts
+++ src/services/verifyToken.ts
@@ -1,13 +1,8 @@
 import { Request } from "express";
 import { JwtPayload } from "jsonwebtoken";
 import { I_User } from "../models/User.model";
-import { callbackController } from "../types";
 
 export interface CustomRequest extends Request {
   token?: string | JwtPayload;
   user: I_User;
 }
-//verify if token exist, then verify through jwt.verify with token and secret key, then send req.user = user, next()
-export const verifyUser = ({ req, res, next }: callbackController) => {};
-
-export const verifyAdmin = ({ req, res, next }: callbackController) => {};
